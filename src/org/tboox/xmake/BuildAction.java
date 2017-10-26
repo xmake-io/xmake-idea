95c9ae6d18786b0a5b155e962ff689297c5566d8
ruki <waruqi@gmail.com>
2017-10-27 00:35:23 +0800

    init project

---
+++ src/org/tboox/xmake/BuildAction.java
@@ -0,0 +1,12 @@
+package org.tboox.xmake;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class BuildAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
