18bc5d610bde21b124f41036a658269dd2c557ab
ruki <waruqi@gmail.com>
2017-10-28 00:58:11 +0800

    add meta-inf for service

--- xmake-service/src/org/tboox/xmake/service/build/XMakeBuilderService.java
+++ xmake-service/src/org/tboox/xmake/service/build/XMakeBuilderService.java
@@ -1,5 +1,6 @@
 package org.tboox.xmake.service.build;
 
+import com.intellij.openapi.diagnostic.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.jps.builders.BuildTargetType;
 import org.jetbrains.jps.incremental.BuilderService;
@@ -10,13 +11,18 @@ import java.util.*;
 
 public class XMakeBuilderService extends BuilderService {
 
+    // the logger
+    private static final Logger Log = Logger.getInstance(XMakeBuilderService.class.getName());
+
     @NotNull
     public List<? extends BuildTargetType<?>> getTargetTypes() {
+        Log.info("getTargetTypes");
         return Arrays.asList(XMakeBuildTargetType.INSTANCE);
     }
 
     @NotNull
     public List<? extends TargetBuilder<?, ?>> createBuilders() {
+        Log.info("createBuilders");
         return Arrays.asList(new XMakeBuilder());
     }
 }
