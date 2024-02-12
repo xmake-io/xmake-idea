adbc2934d14da604c710464810cd7724abe77c92
Seedking <875828815@qq.com>
2024-02-12 19:06:30 +0800

    cleanup code and fix
    Cleanup code with plugin.xml and gradle.kts
    Fix the issue of inability to view documents within the plugin project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,1 @@
-
-pluginManagement {
-    repositories {
-        maven {
-            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
-        }
-        gradlePluginPortal()
-    }
-}
 rootProject.name = "xmake-idea"
-include("clion")
