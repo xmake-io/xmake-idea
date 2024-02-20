694f4422c1bcb3ec4397239cc24b201ba90572ef
Seedking <875828815@qq.com>
2024-02-20 19:06:04 +0800

    Shorten var names

--- src/main/kotlin/io/xmake/utils/interact/LocalData.kt
+++ src/main/kotlin/io/xmake/utils/interact/LocalData.kt
@@ -1,8 +1,14 @@
 package io.xmake.utils.interact
 
 
-val kSystemEnv: MutableMap<String, String>?
+val kSysEnv: MutableMap<String, String>?
     get() = System.getenv()
 
-val kLineSeparator: String
+val kLineSep: String
     get() = System.lineSeparator()
+
+val kSysLang: String
+    get() = System.getProperty("user.language")
+
+val kSysThreadCount: Int
+    get() = Runtime.getRuntime().availableProcessors()
