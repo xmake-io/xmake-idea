49d78b6212ecdce1d8d3d5b448e4c3e1a88beb1a
windchargerj <72346692+windchargerj@users.noreply.github.com>
2024-08-02 16:47:58 +0800

    add new xmake toolkit model

---
+++ src/main/kotlin/io/xmake/project/toolkit/ToolkitHost.kt
@@ -0,0 +1,51 @@
+package io.xmake.project.toolkit
+
+import com.intellij.execution.wsl.WSLDistribution
+import com.intellij.execution.wsl.WslDistributionManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.SystemInfo
+import com.intellij.ssh.config.unified.SshConfig
+import com.intellij.ssh.config.unified.SshConfigManager
+import com.intellij.util.xmlb.annotations.Attribute
+import com.intellij.util.xmlb.annotations.Tag
+import io.xmake.project.toolkit.ToolkitHostType.*
+import kotlinx.coroutines.coroutineScope
+
+@Tag("toolkitLocation")
+data class ToolkitHost(
+    @Attribute
+    val type: ToolkitHostType = LOCAL,
+) {
+
+    constructor(type: ToolkitHostType, target: Any? = null) : this(type) {
+        this.target = target
+        this.id = when (type) {
+            LOCAL -> SystemInfo.getOsName()
+            WSL -> (target as WSLDistribution).id
+            SSH -> (target as SshConfig).id
+        }
+    }
+
+    @Transient
+    var target: Any? = null
+        private set
+
+    @Attribute
+    private var id: String? = null
+
+    suspend fun loadTarget(project: Project? = null) {
+        when (type) {
+            LOCAL -> {}
+            WSL -> loadWslTarget()
+            SSH -> loadSshTarget(project)
+        }
+    }
+
+    private suspend fun loadWslTarget() = coroutineScope {
+        target = WslDistributionManager.getInstance().installedDistributions.find { it.id == id }!!
+    }
+
+    private suspend fun loadSshTarget(project: Project? = null) = coroutineScope {
+        target = SshConfigManager.getInstance(project).findConfigById(id!!)!!
+    }
+}
---
+++ src/main/kotlin/io/xmake/project/toolkit/ToolkitHostType.kt
@@ -0,0 +1,5 @@
+package io.xmake.project.toolkit
+
+enum class ToolkitHostType {
+    LOCAL, WSL, SSH,
+}
