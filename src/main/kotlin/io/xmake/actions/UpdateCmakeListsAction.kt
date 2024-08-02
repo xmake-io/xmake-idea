49118f2c5df70ac298e02493f009e053498287d4
windchargerj <72346692+windchargerj@users.noreply.github.com>
2024-08-03 00:40:29 +0800

    add sync operation when generate from wsl or remote

--- src/main/kotlin/io/xmake/actions/UpdateCmakeListsAction.kt
+++ src/main/kotlin/io/xmake/actions/UpdateCmakeListsAction.kt
@@ -1,13 +1,19 @@
 package io.xmake.actions
 
+import com.intellij.execution.RunManager
 import com.intellij.execution.process.ProcessAdapter
 import com.intellij.execution.process.ProcessEvent
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.vfs.LocalFileSystem
+import io.xmake.project.toolkit.activatedToolkit
 import io.xmake.project.xmakeConsoleView
+import io.xmake.run.XMakeRunConfiguration
 import io.xmake.shared.xmakeConfiguration
 import io.xmake.utils.SystemUtils
+import io.xmake.utils.execute.SyncDirection
+import io.xmake.utils.execute.syncFileByToolkit
+import kotlinx.coroutines.GlobalScope
 
 class UpdateCmakeListsAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
@@ -22,12 +28,25 @@ class UpdateCmakeListsAction : AnAction() {
         if (xmakeConfiguration.changed) {
             SystemUtils.runvInConsole(project, xmakeConfiguration.configurationCommandLine).addProcessListener(object: ProcessAdapter() {
                 override fun processTerminated(e: ProcessEvent) {
-                    SystemUtils.runvInConsole(project, xmakeConfiguration.updateCmakeListsCommandLine, false, true, true)
+                    SystemUtils.runvInConsole(project, xmakeConfiguration.updateCmakeListsCommandLine, false, true, true).addProcessListener(
+                        object: ProcessAdapter() {
+                            override fun processTerminated(e: ProcessEvent) {
+                                syncFileByToolkit(GlobalScope, project, project.activatedToolkit!!, "CMakeLists.txt", SyncDirection.UPSTREAM_TO_LOCAL)
+                                // Todo: Reload from disks after download from remote.
+                            }
+                        }
+                    )
                 }
             })
             xmakeConfiguration.changed = false
         } else {
-            SystemUtils.runvInConsole(project, xmakeConfiguration.updateCmakeListsCommandLine, true, true, true)
+            SystemUtils.runvInConsole(project, xmakeConfiguration.updateCmakeListsCommandLine, false, true, true).addProcessListener(
+                object: ProcessAdapter() {
+                    override fun processTerminated(e: ProcessEvent) {
+                        syncFileByToolkit(GlobalScope, project, project.activatedToolkit!!, "CMakeLists.txt", SyncDirection.UPSTREAM_TO_LOCAL)
+                    }
+                }
+            )
         }
     }
 }
--- src/main/kotlin/io/xmake/actions/UpdateCompileCommandsAction.kt
+++ src/main/kotlin/io/xmake/actions/UpdateCompileCommandsAction.kt
@@ -4,9 +4,13 @@ import com.intellij.execution.process.ProcessAdapter
 import com.intellij.execution.process.ProcessEvent
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import io.xmake.project.toolkit.activatedToolkit
 import io.xmake.project.xmakeConsoleView
 import io.xmake.shared.xmakeConfiguration
 import io.xmake.utils.SystemUtils
+import io.xmake.utils.execute.SyncDirection
+import io.xmake.utils.execute.syncFileByToolkit
+import kotlinx.coroutines.GlobalScope
 
 class UpdateCompileCommandsAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
@@ -21,12 +25,25 @@ class UpdateCompileCommandsAction : AnAction() {
         if (xmakeConfiguration.changed) {
             SystemUtils.runvInConsole(project, xmakeConfiguration.configurationCommandLine).addProcessListener(object: ProcessAdapter() {
                 override fun processTerminated(e: ProcessEvent) {
-                    SystemUtils.runvInConsole(project, xmakeConfiguration.updateCompileCommansLine, false, true, true)
+                    SystemUtils.runvInConsole(project, xmakeConfiguration.updateCompileCommansLine, false, true, true).addProcessListener(
+                        object: ProcessAdapter() {
+                            override fun processTerminated(e: ProcessEvent) {
+                                syncFileByToolkit(GlobalScope, project, project.activatedToolkit!!, "compile_commands.json", SyncDirection.UPSTREAM_TO_LOCAL)
+                                // Todo: Reload from disks after download from remote.
+                            }
+                        }
+                    )
                 }
             })
             xmakeConfiguration.changed = false
         } else {
-            SystemUtils.runvInConsole(project, xmakeConfiguration.updateCompileCommansLine, true, true, true)
+            SystemUtils.runvInConsole(project, xmakeConfiguration.updateCompileCommansLine, false, true, true).addProcessListener(
+                object: ProcessAdapter() {
+                    override fun processTerminated(e: ProcessEvent) {
+                        syncFileByToolkit(GlobalScope, project, project.activatedToolkit!!, "compile_commands.json", SyncDirection.UPSTREAM_TO_LOCAL)
+                    }
+                }
+            )
         }
     }
 }
