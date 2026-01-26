8e999f0d186153f78ebb6c025e3941c3338e293a
ruki <waruqi@gmail.com>
2026-01-26 22:41:16 +0800

    enable intellij logger

--- clion-debug/src/main/kotlin/io/xmake/debug/clion/utils/Logger.kt
+++ clion-debug/src/main/kotlin/io/xmake/debug/clion/utils/Logger.kt
@@ -27,91 +27,91 @@ import java.time.format.DateTimeFormatter
  * Simple logger implementation for CLion debug module
  */
 object Logger {
-    
+
     private const val DEFAULT_TAG = "ClionDebugModule"
-    
+
     // Log levels
     enum class LogLevel {
         DEBUG,
-        VERBOSE, 
+        VERBOSE,
         INFO,
         WARN,
         ERROR
     }
-    
+
     // Current log level (can be configured)
     private var currentLogLevel = LogLevel.DEBUG
-    
+
     /**
      * Set the current log level
      */
     fun setLogLevel(level: LogLevel) {
         currentLogLevel = level
     }
-    
+
     /**
      * Debug level log with default tag
      */
     fun d(message: String) {
         d(DEFAULT_TAG, message)
     }
-    
+
     /**
      * Debug level log with tag
      */
     fun d(tag: String, message: String) {
         log(LogLevel.DEBUG, tag, message)
     }
-    
+
     /**
      * Verbose level log with default tag
      */
     fun v(message: String) {
         v(DEFAULT_TAG, message)
     }
-    
+
     /**
      * Verbose level log with tag
      */
     fun v(tag: String, message: String) {
         log(LogLevel.VERBOSE, tag, message)
     }
-    
+
     /**
      * Info level log with default tag
      */
     fun i(message: String) {
         i(DEFAULT_TAG, message)
     }
-    
+
     /**
      * Info level log with tag
      */
     fun i(tag: String, message: String) {
         log(LogLevel.INFO, tag, message)
     }
-    
+
     /**
      * Warning level log with default tag
      */
     fun w(message: String) {
         w(DEFAULT_TAG, message)
     }
-    
+
     /**
      * Warning level log with tag
      */
     fun w(tag: String, message: String) {
         log(LogLevel.WARN, tag, message)
     }
-    
+
     /**
      * Warning level log with exception and default tag
      */
     fun w(message: String, throwable: Throwable) {
         w(DEFAULT_TAG, message, throwable)
     }
-    
+
     /**
      * Warning level log with exception and tag
      */
@@ -119,28 +119,28 @@ object Logger {
         log(LogLevel.WARN, tag, message)
         throwable.printStackTrace()
     }
-    
+
     /**
      * Error level log with default tag
      */
     fun e(message: String) {
         e(DEFAULT_TAG, message)
     }
-    
+
     /**
      * Error level log with tag
      */
     fun e(tag: String, message: String) {
         log(LogLevel.ERROR, tag, message)
     }
-    
+
     /**
      * Error level log with exception and default tag
      */
     fun e(message: String, throwable: Throwable) {
         e(DEFAULT_TAG, message, throwable)
     }
-    
+
     /**
      * Error level log with exception and tag
      */
@@ -148,7 +148,7 @@ object Logger {
         log(LogLevel.ERROR, tag, message)
         throwable.printStackTrace()
     }
-    
+
     /**
      * Core logging method
      */
@@ -156,12 +156,12 @@ object Logger {
         if (level.ordinal < currentLogLevel.ordinal) {
             return
         }
-        
+
         // Use println for all logging in the debug module
         val timestamp = LocalDateTime.now().format(
             DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
         )
-        
+
         val levelChar = when (level) {
             LogLevel.DEBUG -> "D"
             LogLevel.VERBOSE -> "V"
@@ -169,7 +169,7 @@ object Logger {
             LogLevel.WARN -> "W"
             LogLevel.ERROR -> "E"
         }
-        
+
         println("[$timestamp] $levelChar/$tag: $message")
     }
 }
