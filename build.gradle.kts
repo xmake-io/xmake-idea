6c699919b4d5aa60a45c79efde1b752b68129a83
windchargerj <72346692+windchargerj@users.noreply.github.com>
2026-07-13 22:32:30 +0800

    chore(build): update IntelliJ Platform build plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,9 @@ val localDescription: String = file("${projectDir}/description.html").readText(C
 
 plugins {
     id("java")
-    id("org.jetbrains.intellij.platform") version "2.7.2"
+    id("org.jetbrains.intellij.platform") version "2.18.1"
     id("org.jetbrains.kotlin.jvm") version "2.3.0"
-    id("org.jetbrains.changelog") version "2.2.0"
+    id("org.jetbrains.changelog") version "2.5.0"
     kotlin("plugin.serialization") version "2.3.0"
 }
 
