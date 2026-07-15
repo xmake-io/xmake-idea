84ea68fdb89bd874478243362f27c81d3b9f9837
windchargerj <72346692+windchargerj@users.noreply.github.com>
2026-07-15 15:27:31 +0800

    chore(test): remove unused MockK dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,7 +65,6 @@ tasks.matching { task -> task.name.contains("buildSearchableOptions") }.configur
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
-    testImplementation("io.mockk:mockk:1.13.12")
     testImplementation("junit:junit:4.13.2")
 }
 
