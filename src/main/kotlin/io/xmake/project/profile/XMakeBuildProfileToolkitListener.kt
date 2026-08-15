2e676a2973c1a0e52cb13bca1a33af3665db0d83
windchargerj <72346692+windchargerj@users.noreply.github.com>
2026-08-15 13:02:37 +0800

    feat(profile): persist project build profiles

---
+++ src/main/kotlin/io/xmake/project/profile/XMakeBuildProfileToolkitListener.kt
@@ -0,0 +1,26 @@
+/*!A Xmake integration in IntelliJ IDEA/Clion
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (C) 2015-present, Xmake Open Source Community.
+ */
+package io.xmake.project.profile
+
+import com.intellij.openapi.project.Project
+import io.xmake.project.toolkit.ToolkitListener
+
+class XMakeBuildProfileToolkitListener(private val project: Project) : ToolkitListener {
+    override fun toolkitsChanged() {
+        project.xmakeBuildProfiles.handleToolkitChanges()
+    }
+}
